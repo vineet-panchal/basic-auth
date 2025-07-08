e88bd21927e00d30770e1c64223e763861e7975c
Vineet Panchal <vineetpanchal03@gmail.com>
2025-07-08 01:19:33 -0400

    added auth server

---
+++ authServer.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+app.use(express.json());
+
+let refreshTokens = [];
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401); // if there isn't any token
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403); // if the token is no longer valid
+  
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+  res.sendStatus(204); // No Content
+});
+
+app.post('/login', (req, res) => {
+  // authenticate user
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+
+  refreshTokens.push(refreshToken); // store the refresh token
+
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+}
+
+app.listen(4000);
