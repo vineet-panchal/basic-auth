bb6164898247a8e80b7e9c9c44d2cfb8480c3d2e
Vineet Panchal <vineetpanchal03@gmail.com>
2025-07-07 23:50:33 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+app.use(express.json());
+
+const posts = [
+  {
+    username: 'Jeff',
+    title: 'Post 1', 
+    content: 'Content of Post 1'
+  },
+  {
+    username: 'Vineet',
+    title: 'Post 2', 
+    content: 'Content of Post 2' 
+  },
+  { 
+    username: 'John',
+    title: 'Post 3', 
+    content: 'Content of Post 3' 
+  }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+  
+  res.json(posts);
+});
+
+app.post('/login', (req, res) => {
+  // authenticate user
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+  
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  
+  if (token == null) return res.sendStatus(401); // if there isn't any token
+  
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403); // if the token is no longer valid
+    req.user = user;
+    next(); // pass the execution off to whatever request the client intended
+  });
+}
+
+app.listen(3000);
